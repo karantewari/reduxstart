890b65e669eb01088409c5159efa5058b94d987c
Karan Tewari <hello@karantewari.in>
2018-09-19 02:18:01 +0530

    added counter and styles, no redux yet

---
+++ src/components/CounterTrack/CounterTrack.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CounterTrack = (props) => {
+    return (
+        <div>
+            <h3>Counter Value : {props.counterVal}</h3>
+        </div>
+    )
+}
+
+export default CounterTrack;
